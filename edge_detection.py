7ab5e457a7ff2e65f2f43c76beafbc17b2a9a1e0
Abdullah-i5o <abdullah@ifiveo.com>
2023-08-09 16:22:36 +0500

    added image and video uploader

--- edge_detection.py
+++ edge_detection.py
@@ -1,46 +1,106 @@
 import cv2
 import numpy as np
 import streamlit as st
+import tempfile
 
 types = None
-data = st.selectbox('Select data type', ("IMAGE",'VIDEO'))
+data = st.selectbox('Select data type', ("IMAGE", 'VIDEO'))
 if data == 'IMAGE':
     types = ['png', 'jpg', 'jpeg']
 
+    upload_file = st.file_uploader('Choose an Image File', type=types)
+    if upload_file is not None:
+        raw_bytes = np.asarray(bytearray(upload_file.read()), dtype=np.uint8)
+        img = cv2.imdecode(raw_bytes, cv2.IMREAD_COLOR)
+        input_col, out_col = st.columns(2)
+        with input_col:
+            st.header('Original')
+            st.image(img, channels='BGR')
 
-upload_file = st.file_uploader('Choose an Image File', type=types)
-if upload_file is not None:
-    raw_bytes = np.asarray(bytearray(upload_file.read()), dtype=np.uint8)
-    img = cv2.imdecode(raw_bytes, cv2.IMREAD_COLOR)
+        # st.header('FILTERS')
+        image_filter = st.selectbox('Select image filter:', ('B/W', 'BLUR', 'CANNY', 'PENCIL', 'STYLIZATION'))
 
-    input_col, out_col = st.columns(2)
-    with input_col:
-        st.header('Original')
-        st.image(img, channels='BGR')
+        with out_col:
+            st.header('OUTPUT')
+            # ret, frame = cv2.imread(upl)
+            # frame = cv2.flip(frame, 1)
+            result = None
+            if image_filter == 'BLUR':
+                result = cv2.GaussianBlur(img, (11, 11), 0, 0)
+                color = 'BGR'
+            elif image_filter == 'B/W':
+                result = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                color = 'GRAY'
+            elif image_filter == 'CANNY':
+                img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                blur = cv2.GaussianBlur(img_gray, (5, 5), 0, 0)
+                result = cv2.Canny(blur, 145, 200)
+                color = 'GRAY'
+            elif image_filter == 'PENCIL':
+                blur = cv2.GaussianBlur(img, (11, 11), 0, 0)
+                result, _ = cv2.pencilSketch(blur)
+                color = 'GRAY'
+            elif image_filter == 'STYLIZATION':
+                result = cv2.stylization(img, sigma_s=40, sigma_r=0.1)
+                color = 'BGR'
 
-    # st.header('FILTERS')
-    image_filter = st.selectbox('Select image filter:', ('B/W','BLUR', 'CANNY', 'SOBEL', 'PENCIL'))
+            st.image(result, channels=color)
 
-    with out_col:
-        st.header('OUTPUT')
-        # ret, frame = cv2.imread(upl)
-        # frame = cv2.flip(frame, 1)
-        result = None
-        if image_filter == 'BLUR':
-            result = cv2.GaussianBlur(img, (10, 10), 0, 0)
-        elif image_filter == 'B/W':
-            result = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        elif image_filter == 'CANNY':
-            blur = cv2.GaussianBlur(img, (5, 5), 0, 0)
-            result = cv2.Canny(blur, 145, 180)
-        elif image_filter == 'SOBEL':
-            blur = cv2.GaussianBlur(img, (5, 5), 0, 0)
-            result, _ = cv2.pencilSketch(blur)
-        elif image_filter == 'PENCIL':
-            blur = cv2.GaussianBlur(img, (10, 10), 0, 0)
-            result = cv2.stylization(blur, sigma_s=40, sigma_r=0.1)
+elif data == 'VIDEO':
 
-        st.image(result, channels='BGR')
+    types = ['mp4', 'avi']
+    f = st.file_uploader("Upload file", type=types)
+    if f is not None:
+        tfile = tempfile.NamedTemporaryFile(delete=False)
+        tfile.write(f.read())
+
+        vid_input_col, vid_out_col = st.columns(2)
+
+        with vid_input_col:
+            st.header('Original')
+            video_file = open(tfile.name, 'rb')
+            video_bytes = video_file.read()
+            st.video(video_bytes)
+
+
+        # st.header('FILTERS')
+        image_filter = st.selectbox('Select video filter:', ('B/W', 'BLUR', 'CANNY', 'PENCIL', 'STYLIZATION'))
+
+
+        with vid_out_col:
+            st.header('Output')
+            vf = cv2.VideoCapture(tfile.name)
+            stframe = st.empty()
+
+            while vf.isOpened():
+                ret, frame = vf.read()
+                # if frame is read correctly ret is True
+                if not ret:
+                    print("Can't receive frame (stream end?). Exiting ...")
+                    break
+                img = frame
+                result = None
+                if image_filter == 'BLUR':
+                    result = cv2.GaussianBlur(img, (11, 11), 0, 0)
+                    color = 'BGR'
+                elif image_filter == 'B/W':
+                    result = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                    color = 'GRAY'
+                elif image_filter == 'CANNY':
+                    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                    blur = cv2.GaussianBlur(img_gray, (5, 5), 0, 0)
+                    result = cv2.Canny(blur, 145, 200)
+                    color = 'GRAY'
+                elif image_filter == 'PENCIL':
+                    blur = cv2.GaussianBlur(img, (11, 11), 0, 0)
+                    result, _ = cv2.pencilSketch(blur)
+                    color = 'GRAY'
+                elif image_filter == 'STYLIZATION':
+                    result = cv2.stylization(img, sigma_s=40, sigma_r=0.1)
+                    color = 'BGR'
+
+                stframe.image(result,channels=color)
+
 
     # if cv2.waitKey(1) == ord('q'):
     #     break
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import streamlit as st
+import cv2
+import tempfile
 
+f = st.file_uploader("Upload file")
+tfile = tempfile.NamedTemporaryFile(delete=False)
+tfile.write(f.read())
+vf = cv2.VideoCapture(tfile.name)
+stframe = st.empty()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+while vf.isOpened():
+    ret, frame = vf.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
